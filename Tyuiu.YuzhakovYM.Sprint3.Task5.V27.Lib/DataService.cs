bff09c5c2b15075cacefac8c807a56e18c9eab84
Tyuiu.YuzhakovYM@outlook.com <yuzhakov3@inbox.ru>
2023-10-24 20:15:48 +0500

    Task5

---
+++ Tyuiu.YuzhakovYM.Sprint3.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.YuzhakovYM.Sprint3.Task5.V27.Lib
+{
+    public class DataService : ISprint3Task5V27
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, k;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries += Math.Pow(k, 2) / Math.Cos(k);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.YuzhakovYM.Sprint3.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YuzhakovYM.Sprint3.Task5.V27.Lib;
+
+namespace Tyuiu.YuzhakovYM.Sprint3.Task5.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+
+            double wait = -1495.954;
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
